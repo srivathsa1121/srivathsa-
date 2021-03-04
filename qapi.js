5db864e9b427e093f201b6817b962450f6f95129
srivathsa1121 <78403467+srivathsa1121@users.noreply.github.com>
2021-03-04 21:01:30 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6040fcdb83b4ad001e354d36",
+				"6040fcdb83b4ad001e354d38"
+			]
+		},
+		"6040fcdb83b4ad001e354d36": {
+			"id": "6040fcdb83b4ad001e354d36",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6040fcdb83b4ad001e354d38": {
+			"id": "6040fcdb83b4ad001e354d38",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
